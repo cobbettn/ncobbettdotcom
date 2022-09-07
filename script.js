e3cb41a7f811721b1cf6bf384c0fce9cd988da7a
Nathan Cobbett <cobbett.n@gmail.com>
2022-09-07 14:43:04 -0400

    adds script for pseudo navigation, adds styling, updates ids in markup

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+    addNavListeners();
+    navigate('home');
+}
+
+const navigate = (page) => {
+    selectNavLink(page);
+    const { content } = document.querySelector(`#${page}Template`);
+    const contentSection = document.querySelector('#content');
+    contentSection.replaceChildren();
+    contentSection.appendChild(content.cloneNode(true));
+}
+
+const selectNavLink = (page) => {
+    document.querySelectorAll('nav a').forEach(link => link.style.textDecoration = link.id === page ? 'underline' : 'none');
+}
+
+const addNavListeners = () => {
+    document.querySelectorAll('nav a').forEach(link => link.addEventListener('click', () => navigate(link.id)));
+}
