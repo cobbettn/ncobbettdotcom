527fe9f4b06b652b34cf176e3583a0c083209ef5
Nathan Cobbett <cobbett.n@gmail.com>
2022-11-07 18:54:08 -0500

    bio, photos, styles

--- public/js/photos.js
+++ public/js/photos.js
@@ -1,17 +1,44 @@
-window.onload = () => {
-    const totalPhotos = 27; // update this count when photos are added/removed
-    let currentPhotoIndex = 1;
-    const currentPhoto = document.querySelector('#currentPhoto');
-    document.querySelector('#nextPhoto').addEventListener('click', () => {
-        if (currentPhotoIndex < totalPhotos) {
-            currentPhotoIndex++;
-            currentPhoto.src = `public/assets/photos/${currentPhotoIndex}.jpg`;
-        }
-    });
-    document.querySelector('#prevPhoto').addEventListener('click', () => {
-        if (currentPhotoIndex > 1) {
-            currentPhotoIndex--;
-            currentPhoto.src = `public/assets/photos/${currentPhotoIndex}.jpg`;
-        }
-    });
+const totalPhotos = 27; // update this count when photos are added/removed
+const thumbnails = 4;
+let thumbnailPage = 0;
+let photo = 0;
+
+const updateThumbnails = () => {
+    const index = thumbnailPage * thumbnails;
+    const thumbnailContainer = document.querySelector('#thumbnails');
+    thumbnailContainer.replaceChildren();
+    for (let i = index; i < index + thumbnails && i < totalPhotos; i++) {
+        const img = document.createElement('img');
+        img.src = `public/assets/photos/${i + 1}.jpg`;
+        thumbnailContainer.appendChild(img);
+    }
 }
+
+
+const currentPhoto = document.querySelector('#currentPhoto');
+document.querySelector('#nextPhoto').addEventListener('click', () => {
+    if (photo < totalPhotos - 1) {
+        photo++;
+        currentPhoto.src = `public/assets/photos/${photo + 1}.jpg`;
+    }
+});
+document.querySelector('#prevPhoto').addEventListener('click', () => {
+    if (photo > 0) {
+        photo--;
+        currentPhoto.src = `public/assets/photos/${photo + 1}.jpg`;
+    }
+});
+document.querySelector('#nextThumbnail').addEventListener('click', () => {
+    if ((thumbnailPage * thumbnails) + thumbnails < totalPhotos) {
+        thumbnailPage++;
+        updateThumbnails();
+    }
+});
+document.querySelector('#prevThumbnail').addEventListener('click', () => {
+    if (thumbnailPage * thumbnails > 0) {
+        thumbnailPage--;
+        updateThumbnails();
+    }
+});
+updateThumbnails();
+
